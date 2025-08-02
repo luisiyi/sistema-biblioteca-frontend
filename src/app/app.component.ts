85edd80b9ef7a872361347dbd6c25176dd962112
LAGM <luisiyi1997@gmail.com>
2025-08-01 22:05:56 -0700

    The book-lista component created
    -added the "book-list.component.html" beside the "ts" files with the following command "ng g c book-list --skip-tests" (BookListComponent)
    -added <app-book-list> selector in "app.component.html" file, this is useful to reuse it in different part of the application.
    -deleted the "book-list.component.css" file because we didn´t need it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BookListComponent } from "./book-list/book-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [BookListComponent]
 })
 export class AppComponent {
   title = 'biblioteca-app';
---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-list.component.html'
+})
+export class BookListComponent {
+
+}
