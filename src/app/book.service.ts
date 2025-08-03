8274734e1c2d9d477ed6c5b00a2e85ba0b821999
LAGM <luisiyi1997@gmail.com>
2025-08-02 21:25:41 -0700

    Book Service added and Http allowed to request in the Front-end
    -updated the "app.config.ts" file in it was added the "provideHttpClient()" provider, this is important to request from the Fronend.
    -added the "book.service.ts" file with the following command "ng g s book --skip-tests", in it was declared the urlBase that request all the books and other variable that inject the HttpClient, also added the "getBookList()" method that return an observable of Book.

---
+++ src/app/book.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Book } from './book';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+//ng g s book --skip-tests
+export class BookService {
+
+  private urlBase = "http://localhost:8080/bookstore-app/books";
+  private clientHttp = inject(HttpClient);
+
+  getBookList(): Observable<Book[]>{
+    return this.clientHttp.get<Book[]>(this.urlBase)
+  }
+}
