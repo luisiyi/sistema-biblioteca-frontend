d17afdac53281398f9d04a01888e5e52fd0643bf
LAGM <luisiyi1997@gmail.com>
2025-08-04 20:23:40 -0700

    getBooks method in book-list.component TS file
    -added two attributes 1. "books!: Books[];" that is an array of Books class and it will initialaze in the future, then "private bookService = inject(BookService)" this one is an dependency injection of the "BookService" class.
    -added the "ngOnInit()" method, this clause will act when the component is loaded instantly the data will load in that component, this method call the "getBooks()" method.
    -"getBooks()" method here we call the "this.bookService.getBookList().subscribe" service that is injected in "bookService" variable, if we receive data then we initialize the "books" attribute with the data retrieved from the service, if we get an error we just log in the console the error.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Book } from '../book';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -7,5 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './book-list.component.html'
 })
 export class BookListComponent {
+  books!: Book[];
+
+  private bookService = inject(BookService)
+
+  ngOnInit(){
+    //load the books
+    this.getBooks();
+  }
+
+  private getBooks(): void{
+    this.bookService.getBookList().subscribe(
+      {
+        next: (data) =>{
+          this.books = data;
+        },
+        error: (error) => {
+          console.error("Error at getting the books", error)
+        }
+      }
+    )
+  }
 
 }
