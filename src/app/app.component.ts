343b87f10e5797eef64bc6c00ec5ebd5a6af7d77
LAGM <luisiyi1997@gmail.com>
2025-08-06 21:18:50 -0700

    Routes and redirection in Angular
    -first we setup the routes in the "app.routes.ts" file, we just set "/books" route by default and if the user set empty after urlBase, this one will redirect to the "/books" anyway.
    -the "routerModule" is very IMPORTANT because Angular cannot recognize and manage the routing, without this also cannot interpret "<router-outlet>" and the "routerLink", so we should import "RouterModule" in the "app.component.ts" file instead calling the components directly.
    -with all this set before, now we can route the "app.component.html" file, we replace the (href="") with (routerLink="") we replace it because href recharge the page completly and lost the app´s status, (routerLinkActive="active") is for make a highligh in the active tab.
    -<router-outlet> is for rendering dinamically the components based on the current route in the URL.
    - for routing works, we need to define the "routes" in the "app.config.ts", although this setup is by default. "provideRouter(routes)" this enables the routes in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BookListComponent } from "./book-list/book-list.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [BookListComponent]
+  imports: [RouterModule] //Replaced from "BookListComponent" to "RouterModule" instead, this will recognize the "routerLink" and <router-outlet>
 })
 export class AppComponent {
   title = 'biblioteca-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'books', component: BookListComponent},//these paths are used in the view/html
+    {path: '', redirectTo: 'books', pathMatch: 'full'}//these paths are used in the view/html
+];
