5f9266dcfe9b006275b53f517d172b31449825f4
LAGM <luisiyi1997@gmail.com>
2025-08-11 21:54:18 -0700

    Add Book service beside in the component ts
    summary: first service, then component, and finally the html view.
    _____**SERVICE:
    -added the "addBook(book: Book): Observable<Object>" method in the "book.service.ts" class, this we use the "clienteHttp" to post to the baseURL the book received in the method, this method return the Observable object.
    _____**COMPONENT:
    -updated the "add-book.component.ts" file, added the book attribute and initializated with new Book();, then injected the "bookService" from the "BookService", also injected the "router" from the "Router", also added the "onSubmit" method
    -added the "saveBook" method, we subscribe here from our service, this method receive the "book" recently created and intializated in the previous point mentioned, this method redirect to the "/books" thankfully for the "goToBooksList" method.
    in this component is very IMPORTANT to have "FormsModule" in the array imports because this allows "two way data binding[ngModel]" in Angular between the component and the view.
    _____**HTML/VIEW:
    -added the (ngSubmit)=on"onSubmit()" in the form from the "add-book.component.html" file this avoid recharge the page and instead runs the "onSubmit" method in the "component", also we added the "name" property and "[(ngModel)]="book.property" in each input because this links each input with the object´s property, allowing the changes in the form directly in the variable.

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Book } from '../book';
+import { BookService } from '../book.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], //Two Way Data Binding
   templateUrl: './add-book.component.html'
 })
 export class AddBookComponent {
+  book: Book = new Book();
+
+  private bookService = inject(BookService);
+  private router = inject(Router);
+
+  onSubmit(){
+    this.saveBook();
+  }
+
+  saveBook(){
+    this.bookService.addBook(this.book).subscribe({
+      next: (datos) => {
+        this.goToBooksList();
+      },
+      error: (error: any) => {console.log(error)}
+    });
+  }
+
+  goToBooksList(){
+    this.router.navigate(['/books'])
+  }
 
 }
